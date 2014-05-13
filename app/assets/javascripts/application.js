9420546845748f26eea9a201ab3afe41a8bae824
Michal Vich <vich@zensys.cz>
2014-05-13 17:55:59 +0200

    set up angularjs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
+//= require angular
+//= require angular-resource
+//= require angular/main.js
+//= require_tree ./angular
 //= require_tree .
